8c0cb17c72d29456399e0edcb6cb83b6b0ebf00d
MCookson2 <matt.cookson2@gmail.com>
2020-08-28 19:22:56 +0100

    add function to change style of list item after clicking it

--- script.js
+++ script.js
@@ -7,8 +7,17 @@ let list = document.getElementById('todo-items')
 function addListItem() {
     let li = document.createElement('li')
     li.appendChild(document.createTextNode(userInput.value));
+    li.setAttribute('class', 'listItem')
+
+    function changeCompleted() {    // function to change style of li after it's clicked.  Toggleable so if hit by mistake can undo.
+        li.classList.toggle('completed')
+    }
+
+    li.addEventListener('click', changeCompleted)
+    
     list.appendChild(li)
-    userInput.value = ''   
+    userInput.value = ''
+
 }
 
 function checkEmpty() {
@@ -17,8 +26,8 @@ function checkEmpty() {
     }
 }
 
-
 submitButton.addEventListener('click', checkEmpty)
+
 userInput.addEventListener('keyup', function(event) {
     if (event.keyCode === 13) {
         checkEmpty()
